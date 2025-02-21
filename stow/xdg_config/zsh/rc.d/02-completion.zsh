307a6ab430c3cbb50a1b302c8d3e9aec9cd5f4e2
NOGUCHI Shoji <github.com@jpng.jp>
2025-02-21 10:53:51 +0900

    zsh: add cargo environment setup and improve completion keybindings

--- stow/xdg_config/zsh/rc.d/02-completion.zsh
+++ stow/xdg_config/zsh/rc.d/02-completion.zsh
@@ -75,12 +75,6 @@ zstyle ':completion:*' file-sort change reverse
 # Completion Matching Control
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
-zmodload zsh/complist
-bindkey -M menuselect 'h' vi-backward-char
-bindkey -M menuselect 'k' vi-up-line-or-history
-bindkey -M menuselect 'j' vi-down-line-or-history
-bindkey -M menuselect 'l' vi-forward-char
-
 # npm completion
 if [ command -v npm >/dev/null 2>&1 ]; then
     eval "npm completion" > /dev/null
--- stow/xdg_config/zsh/rc.d/03-bindkey.zsh
+++ stow/xdg_config/zsh/rc.d/03-bindkey.zsh
@@ -3,3 +3,11 @@ bindkey -d
 
 # Enable Emacs keybind
 bindkey -e
+
+zmodload zsh/complist
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+
+compinit -u
