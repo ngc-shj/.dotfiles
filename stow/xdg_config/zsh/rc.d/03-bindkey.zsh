14a9c444d2681d89cbce68a0af715f5e7b8ca573
NOGUCHI Shoji <github.com@jpng.jp>
2025-02-21 12:16:30 +0900

    zsh: remove redundant compinit command from keybindings configuration

--- stow/xdg_config/zsh/rc.d/03-bindkey.zsh
+++ stow/xdg_config/zsh/rc.d/03-bindkey.zsh
@@ -9,5 +9,3 @@ bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'j' vi-down-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
-
-compinit -u
